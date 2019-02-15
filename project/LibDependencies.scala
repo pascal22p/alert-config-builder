3a05471a2bedba9ea87817ed616b4564b9d0bfae
José Miguel <474643+jm2dev@users.noreply.github.com>
2019-02-15 16:10:20 +0000

    PLATOPS-1930 deps upgrade.

--- project/LibDependencies.scala
+++ project/LibDependencies.scala
@@ -5,15 +5,15 @@ object LibDependencies {
   val compile = Seq(
     "org.scala-lang" % "scala-reflect" % "2.11.7",
     "org.reflections" % "reflections" % "0.9.9-RC1",
-    "io.spray" %% "spray-json" % "1.3.2",
+    "io.spray" %% "spray-json" % "1.3.5",
     "org.yaml" % "snakeyaml" % "1.17"
   )
 
 
   val test = Seq(
-    "org.json4s" %% "json4s-jackson" % "3.6.0" % "test",
-    "org.scalatest" %% "scalatest" % "2.2.4" % "test",
-    "org.pegdown" % "pegdown" % "1.5.0" % "test"
+    "org.json4s" %% "json4s-jackson" % "3.6.4" % "test",
+    "org.scalatest" %% "scalatest" % "2.2.6" % "test",
+    "org.pegdown" % "pegdown" % "1.6.0" % "test"
   )
 
 }
